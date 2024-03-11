bbc0177c32cc3c5d0c39652421a2903672cbcb01
Balaguru <balaguru.cse@gmail.com>
2024-03-11 23:46:25 +0530

    Add files via upload

---
+++ hello1.cs
@@ -0,0 +1,6 @@
+[Function("HttpFunction")]
+public IActionResult Run(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+{
+    return new OkObjectResult($"Welcome to Azure Functions, {req.Query["name"]}!");
+}
